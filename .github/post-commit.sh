f5e98de96a5409a8316b950a3c82f75aded52ae4
Fergus In London <fergus@fergus.london>
2020-11-24 08:56:49 +0000

    incidental: more repository hygiene (tidy ignore rules)

--- .github/post-commit.sh
+++ .github/post-commit.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-# Post Commit Hook: This hook ensures that the datasheets are up-to-date
-#
-#
+# Post Commit Hook: This hook ensures that the datasheets are up-to-date.
 
+DATASHEETS_DIR="hardware/datasheets"
 DATASHEETS_ZIP="hardware/datasheets.zip"
 COMMIT_MSG="Datasheets: Include updated datasheets archive"
 COMMIT_CFG="advice.ignoredHook=false"
@@ -13,13 +12,18 @@ COMMIT_CFG="advice.ignoredHook=false"
 # -x flag on the hook. We don't want post-commit calling post-commit!
 POSTCOMMIT_HOOK=".git/hooks/post-commit"
 
-echo "checking for changes to the datasheet directory"
-if [[ $(make $DATASHEETS_ZIP) != *"is up to date"* ]]; then
-    echo "[post-commit] detected updates to datasheet directory; made new archive."
-    echo "[post-commit] commiting datasheet archive to git repository"
+# Check we have a datasheets directory - a clean pull without extraction
+# - will not! Then run the make command to detect any changes, if present
+# add the new archive and commit it.
+if [ -d $DATASHEETS_DIR ]; then
+    echo "checking for changes to the datasheet directory"
+    if [[ $(make $DATASHEETS_ZIP) != *"is up to date"* ]]; then
+        echo "[post-commit] detected updates to datasheet directory; made new archive."
+        echo "[post-commit] commiting datasheet archive to git repository"
 
-    [ -x $POSTCOMMIT_HOOK ] && chmod -x $POSTCOMMIT_HOOK
-    git add -f $DATASHEETS_ZIP; git -c "${COMMIT_CFG}" commit -m "${COMMIT_MSG}" -o $DATASHEETS_ZIP
-    chmod +x $POSTCOMMIT_HOOK
-    echo "[post-commit] commited new datasheets.zip"
+        [ -x $POSTCOMMIT_HOOK ] && chmod -x $POSTCOMMIT_HOOK
+        git add -f $DATASHEETS_ZIP; git -c "${COMMIT_CFG}" commit -m "${COMMIT_MSG}" -o $DATASHEETS_ZIP
+        chmod +x $POSTCOMMIT_HOOK
+        echo "[post-commit] commited new datasheets.zip"
+    fi
 fi
